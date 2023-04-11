78223956a29e22f4893114b2f8ee44fb507b7c93
Sahil-2703 <117385977+Sahil-2703@users.noreply.github.com>
2023-04-11 21:24:52 +0530

    Project completed 11-04-2023

---
+++ client/src/components/Fileupload.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import axios from "axios";
+const Fileupload = ({ contract, account, provider }) => {
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState("No image selected");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (file) {
+      try {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        const resFile = await axios({
+          method: "post",
+          url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          data: formData,
+          headers: {
+            pinata_api_key: `
+            826239a29e241640b568`,
+            pinata_secret_api_key: `
+            e6a566c694b78754b47886e0c7139eba9a1ec573fce1d6195e30766d3604e024`,
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        const ImgHash = `ipfs://${resFile.data.IpfsHash}`;
+        //const signer = contract.connect(provider.getSigner());
+        const signer = contract.connect(provider.getSigner());
+        signer.add(account, ImgHash);
+      } catch (e) {
+        alert("Unable to upload image to Pinata");
+      }
+    }
+    alert("Successfully Image Uploaded");
+    setFileName("No image selected");
+    setFile(null);
+  };
+  const retrieveFile = (e) => {
+    const data = e.target.files[0]; //files array of files object
+    // console.log(data);
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(data);
+    reader.onloadend = () => {
+      setFile(e.target.files[0]);
+    };
+    setFileName(e.target.files[0].name);
+    e.preventDefault();
+  };
+  return (
+    <div className="top">
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="file-upload" className="choose">
+          Choose Image
+        </label>
+        <input
+          disabled={!account}
+          type="file"
+          id="file-upload"
+          name="data"
+          onChange={retrieveFile}
+        />
+        <span className="textArea">Image: {fileName}</span>
+        <button type="submit" className="upload" disabled={!file}>
+          Upload File
+        </button>
+      </form>
+    </div>
+  );
+};
+export default Fileupload;
+
